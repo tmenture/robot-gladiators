988b0587855c261dcc4570aeb3ce5e3a2eaf03e6
tmenture <thomasoxemail@gmail.com>
2022-03-11 19:52:22 -0500

    input now stores in browswer console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name");
+
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+
+console.log("Our robot's name is " + playerName);
+
+// this creates a funciton named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
