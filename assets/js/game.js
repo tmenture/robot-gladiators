f3c03eb56bcbbd9be8453bc320c0e842c8c0cc9a
tmenture <thomasoxemail@gmail.com>
2022-03-13 01:02:10 -0500

    initial game set up

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,8 @@
 var playerName = window.prompt("What is your robot's name?");
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
@@ -12,36 +14,60 @@ var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Fight Club!");
 
-    //Subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
-    enemyHealth = enemyHealth - playerAttack;
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        //Subtract the value of playerAttack from the value of enemyHealth and use that result to update the value in the enemyHealth variable
+        enemyHealth = enemyHealth - playerAttack;
 
-    // Log a resulting message to console so we know that it worked 
-    console.log (
-        playerName + "attacked" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
-    // check enemys health
-    if (enemyHealth <= 0 ) {
-        window.alert(enemyName + " has died!");
-    }
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
+        // Log a resulting message to console so we know that it worked 
+        console.log (
+            playerName + "attacked" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+     );
+        // check enemys health
+        if (enemyHealth <= 0 ) {
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
 
-    // Subtract the alue of enemyAttack from the value of playerHealth and use that result to update the value in the playerHealth variable
-    playerHealth = playerHealth - enemyAttack;
+        // Subtract the alue of enemyAttack from the value of playerHealth and use that result to update the value in the playerHealth variable
+        playerHealth = playerHealth - enemyAttack;
 
-    // Log a resulting messege to the console so we know that it worked
-    console.log (
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
+        // Log a resulting messege to the console so we know that it worked
+        console.log (
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
 
-    // check player's health 
-    if (playerHealth <= 0) {
-        window.alert(playerName + " has died!");
+        // check player's health 
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");     
+        }
+
+    // if player choses to skip
+    }else if (promptFight === "skip" || promptFight === "SKIP") {
+        // confim player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        // if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            //subtract money from playerMoney for skipping
+            playerMoney = playerMoney - 2;
+        }
+
+        // if no (false), ask question againby running fight() again
+        else {
+            fight();
+        }
+
+        window.alert(playerName + " has chosen to skip the fight!");
+    }else {
+        window.alert("You need to choose a valid option. Try again!");
     }
-    else {
-        window.alert(playerName + " still has " + playerHealth + " health left.");
-    }
+    
 };
 
 fight();
