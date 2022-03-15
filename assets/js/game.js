5f0be278e37f4115bb51b4b53ea1956f22fea677
tmenture <thomasoxemail@gmail.com>
2022-03-15 14:59:48 -0400

    added shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -95,6 +95,16 @@ var startGame = function() {
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
+
+      // if we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+        // asks player if they want to visit the store
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+        //if yes, takes them to the store
+        if (storeConfirm) {
+          shop ();
+        }
+      }
     }
     // if player isn't alive, stop the game
     else {
@@ -121,8 +131,57 @@ var startGame = function() {
       window.alert("Thank you for playing Robot Fight Club! Come back soon!");
     }
   }
+
   // play again
   endGame();
 };
-// start the game when the page loads
+
+var shop = function() {
+  //ask player what they would like to do
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL you health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+
+  switch (shopOptionPrompt) {
+    case "REFILL":
+    case "refill":
+      if (playerMoney >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        // increase health and decrease money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You dont have enough money!");
+      }
+      break;
+
+    case "UPGRADE":
+    case "upgrade":
+      if (playerMoney >= 7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        // increase attack decrease money
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You dont have eough money!");
+      }
+      break;
+
+    case "LEAVE":
+    case "leave":
+      window.alert("Leaving the store.")
+      // do nothing, so function will end
+      break;
+
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      // call shop() again to force player to pick a valid option
+      shop();
+      break;
+  }
+};
+
+// starts the game when the page loads
 startGame();
